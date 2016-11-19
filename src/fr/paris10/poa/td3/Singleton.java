9f2a7215cc48f9aa5816526508ab8845daaa0004
sokhnaSow <maimouna93sow@gmail.com>
2016-11-20 00:22:54 +0100

    Td_Design_Patterns_Singleton

--- src/fr/paris10/poa/td3/Singleton.java
+++ src/fr/paris10/poa/td3/Singleton.java
@@ -4,10 +4,10 @@ package fr.paris10.poa.td3;
  * Created by mouna on 10/10/2016.
  */
 public class Singleton {
-    private static Singleton ourInstance = new Singleton();
+    private static Singleton instance = new Singleton();
 
     public static Singleton getInstance() {
-        return ourInstance;
+        return instance;
     }
 
     private Singleton() {
