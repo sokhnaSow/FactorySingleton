2ba5231f0a7962594eb2cc8dce2504ae41077eff
sokhnaSow <maimouna93sow@gmail.com>
2016-11-20 00:27:40 +0100

    Td_Design_Patterns_Singleton

--- src/fr/paris10/poa/td3/Client.java
+++ src/fr/paris10/poa/td3/Client.java
@@ -1,11 +1,31 @@
 package fr.paris10.poa.td3;
 
+import java.util.Locale;
+
 /**
  * Created by mouna on 10/10/2016.
  */
 public class Client {
 
-    public Client(){
+    Product prod1;
+    ProductFactory factory;
 
+    public Client(){
+        String language = Locale.getDefault().getDisplayLanguage(Locale.ENGLISH);
+        factory = new ProductFactory(language);
+        prod1 = factory.createProduct();
+    }
+
+    public void foo(){
+        Product myProd1;
+        Product myProd2;
+
+        prod1.perform();
+
+        myProd1 = factory.createProduct();
+        myProd1.perform();
+
+        myProd2 = factory.createProduct();
+        myProd2.perform();
     }
 }
