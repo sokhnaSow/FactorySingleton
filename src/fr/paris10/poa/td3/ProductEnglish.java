c0c7156dc7967955b57087e15a928c08207c9a21
sokhnaSow <maimouna93sow@gmail.com>
2016-11-20 00:25:34 +0100

    Td_Design_Patterns_Singleton

---
+++ src/fr/paris10/poa/td3/ProductEnglish.java
@@ -0,0 +1,15 @@
+package fr.paris10.poa.td3;
+
+/**
+ * Created by mouna on 19/11/2016.
+ */
+public class ProductEnglish implements Product {
+
+    public ProductEnglish() {
+    }
+
+    @Override
+    public void perform() {
+        System.out.println("I am a Product !");
+    }
+}
